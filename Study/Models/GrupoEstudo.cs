451fa120a0ee6f9459e9928a3e1e357b7dcedc03
Hugo Capucho <capuchop@live.com>
2017-05-01 01:58:14 -0300

    Update GrupoEstudo.cs

--- Study/Models/GrupoEstudo.cs
+++ Study/Models/GrupoEstudo.cs
@@ -12,7 +12,6 @@ namespace Study.Models
         public virtual DateTime DataInicio { get; set; }
         public virtual string Local { get; set; }
         public virtual string Descricao { get; set; }
-        public virtual bool Privado { get; set; }
         public virtual Disciplina Disciplina { get; set; }
     }
 }
