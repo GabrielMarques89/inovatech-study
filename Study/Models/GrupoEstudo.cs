c406adf3adb481b1c9922f61eb1bd5fab0a3cd43
Hugo Capucho <capuchop@live.com>
2017-05-14 11:23:36 -0300

    Alterações na View e Mapeamento de Grupo

--- Study/Models/GrupoEstudo.cs
+++ Study/Models/GrupoEstudo.cs
@@ -9,7 +9,7 @@ namespace Study.Models
     {
         public virtual string Nome { get; set; }
         public virtual int QuantidadeMaxAlunos { get; set; }
-        public virtual DateTime DataInicio { get; set; }
+        public virtual DateTime DataEncontro { get; set; }
         public virtual string Local { get; set; }
         public virtual string Descricao { get; set; }
         public virtual bool Privado { get; set; }
