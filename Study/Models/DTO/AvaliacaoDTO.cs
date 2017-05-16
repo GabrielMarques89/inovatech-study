d763c71a5f5fa17c5b884c01f67a28d343531d0d
kelvinxs <kelvinxsouza@gmail.com>
2017-05-16 00:59:30 -0300

    Adicionado a avaliação

---
+++ Study/Models/DTO/AvaliacaoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Study.Models.DTO
+{
+    public class AvaliacaoDTO
+    {
+        public virtual long IdAvaliacao { get; set; }
+        public virtual string Texto { get; set; }
+        public virtual long IdAvaliador { get; set; }
+        public virtual string NomeAvaliador { get; set; }
+        public virtual string FotoAvaliador { get; set; }
+        public virtual long IdAvaliado { get; set; }
+        public virtual string NomeAvaliado { get; set; }
+        public virtual long IdGrupo { get; set; }
+        public virtual string NomeGrupo { get; set; }
+        public virtual bool AvaliacaoPositiva { get; set; }
+    }
+}
